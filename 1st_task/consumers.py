fdc422ae346bbdbf534ae2be27bccd80bfd2012b
Kordebalet232 <yurypetrovich2013@mail.ru>
2023-12-23 11:14:05 +0300

    Закончено задание 1

--- 1st_task/consumers.py
+++ 1st_task/consumers.py
@@ -1,6 +1,8 @@
 from confluent_kafka import Producer, Consumer, TopicPartition
 import time
 import sys
+import json
+import pandas as pd
 
 TOPIC_NAME = "devices"
 TIME_TO_SLEEP = 10
@@ -15,16 +17,30 @@ consumer.subscribe([TOPIC_NAME])
 
 try:
     while True:
+        messages = []
         while True:
-            message = consumer.poll(1.0)
+            message = consumer.poll(timeout=1.0)
             if message is None:  # если сообщение отсутствует - переходим к следующей итерации
                 break
             elif not message.error():  # если сообщение получено без ошибок
-                print('Received message: {}'.format(message.value().decode('utf-8')))  # обрабатываем полученное сообщение
+                value = message.value().decode("utf-8")
+                data = json.loads(value)
+                messages.append(data)
+                # print('Received message: {}'.format(message.value().decode('utf-8')))  # обрабатываем полученное сообщение
             else:
                 print('Error occured')
-        
+
+        print(f"Получено {len(messages)} сообщений")
+        if len(messages) > 0:
+            df = pd.DataFrame(messages)
+            print("Среднее по названию датчика: ")
+            mean_by_name = df.groupby("name").value.mean()
+            print(mean_by_name.to_markdown())
+            print("Среднее по типу датчика: ")
+            mean_by_type = df.groupby("measure").value.mean()
+            print(mean_by_type.to_markdown())
         time.sleep(TIME_TO_SLEEP)
+
 except KeyboardInterrupt:
     sys.stderr.write('Aborted by user\n')
 
--- 1st_task/producers.py
+++ 1st_task/producers.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer, Consumer, TopicPartition
+from confluent_kafka import Producer, admin
 import time
 import random
 import json
@@ -11,6 +11,12 @@ measures = [('pressure', (0, 200)),
             ('temperature', (-200, 200)),
             ('moisture', (0, 100)),
             ]
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'[ERROR] Ошибка доставки сообщения: {err}')
+    else:
+        print(f'[INFO] Сообщение успешно доставлено: {msg.topic()}[{msg.partition()}] at offset {msg.offset()}')
 
 producers = []
 for i in range(NUM_DEVICES):
@@ -30,14 +36,27 @@ while True:
 
     data = {
         "name": name,
-        "timestamp": str(time.time()),
-        "measure": str(measure),
-        "value": str(random.uniform(range[0], range[1]))
+        "timestamp": time.time(),
+        "measure": measure,
+        "value": random.uniform(range[0], range[1])
         }
     
-    producer.produce(TOPIC_NAME, value=json.dumps(data, indent=2).encode('utf-8'), callback="localhost:9092")
+    producer.produce(TOPIC_NAME, key=str(time.time()).encode('utf-8'), value=json.dumps(data, indent=2).encode('utf-8'), callback=delivery_report)
+    time.sleep(SLEEPING_TIME)
     producer.flush()
 
     messages_sent += 1
 
-    time.sleep(SLEEPING_TIME)
+# def delete_topic(topic_name):
+#     a = admin.AdminClient({
+#                 "bootstrap.servers": "localhost:9092"
+#             })
+#     result = a.delete_topics([topic_name])
+#     for topic, future in result.items():
+#         try:
+#             future.result()  # Дождитесь завершения удаления
+#             print(f"Тема {topic} успешно удалена")
+#         except Exception as e:
+#             print(f"Ошибка при удалении темы {topic}: {str(e)}")
+
+# delete_topic("devices")
